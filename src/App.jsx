8a7eff5d4fbc5516732014a6bf15c60c35d434a6
Moritz Rebbert <moritz.rebbert@udo.edu>
2021-08-14 16:35:40 +0200

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react'
 import './App.css'
 import Card from './components/card'
+import LongStack from './components/long_stack'
 
 
 function App({G, ctx}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <h4>Hand</h4>
-        {G.hand[0]}
-        <p>Shopping Card</p>
-          <Card id="h10" open={false}></Card>
-      </header>
+     
+        <div id="hand">
+
+        </div>
+
+        <div id="stacks">
+          {G.stacks
+            .map(stack =>{
+              const {open,close}= stack;
+              const closedCards = close.map(id=>({id, open: false}));
+              const openCards = open.map(id => ({id, open: true}));
+              return [...closedCards, ...openCards]
+            })
+            .map(cards=>{console.log(cards); return cards})
+            .map(cards=><LongStack cards={cards}></LongStack>)}
+        </div>
+
+        <div id="slots">
+
+        </div>
+
+        
     </div>
   )
 }
