0deba3bcd4e172a33c3607a49ae88957442cfc33
Moritz Rebbert <moritz.rebbert@udo.edu>
2021-08-14 18:14:52 +0200

    onMouseDown optional

--- src/components/long_stack.jsx
+++ src/components/long_stack.jsx
@@ -15,7 +15,7 @@ class LongStack extends Component {
               key={card.id}
               id={card.id}
               open={card.open}
-              onMouseDown={((e) => this.props.onMouseDown(e, card.id)) || null}
+              onMouseDown={(e) => this.props.onMouseDown && this.props.onMouseDown(e, card.id) }
               z={index}
               y={index * (this.props.stackOffset || STACK_OFFSET)}></Card>)}
       </div>
