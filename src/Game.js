0821ae1f48addcc7ef30cf9a2793b1892dcb48af
Moritz Rebbert <moritz.rebbert@udo.edu>
2021-08-14 12:42:29 +0200

    example state

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,25 @@
+
+
 export const TicTacToe = {
-    setup: () => ({ cells: Array(9).fill(null) }),
+    setup: () => ({ 
+        deck: ["h1","h2", "h3", "h10", "hjack", "hqueen", "hking"],
+        hand: ["c1"],
+        stacks : [
+            {open: ["d4"], close: []},
+            {open: ["squeen"], close: ["sjack"]},
+            {open: ["hking"], close: ["hqueen", "hking"]},
+            {open: ["s7"], close: ["hking", "h4", "h7"]},
+            {open: ["h9"], close: ["h9", "h5", "h6", "h5", "h6",]},
+            {open: ["c5"], close:  ["h9", "h5", "h6", "h5", "h6", "hking"]},
+            {open: ["s10"], close:  ["h9", "h5", "h6", "h9", "h5", "h6", "h9"]},
+        ],
+        slots : {
+            hearts: ["h1"],
+            clubs: [],
+            spades: ["s2","s1"],
+            diamonds: []
+        }
+    }),
   
     moves: {
       clickCell: (G, ctx, id) => {
